fb58357505055380c347b50d5a479e558b7fab82
uuiid <957714080@qq.com>
2025-07-28 13:33:52 +0800

    调整头

--- src/main/index.ts
+++ src/main/index.ts
@@ -59,6 +59,7 @@ function createWindow(): void {
     // set custom User-Agent in requestHeaders
     details.requestHeaders['User-Agent'] =
       'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36'
+    delete details.requestHeaders['Referer']
     callback({ cancel: false, requestHeaders: details.requestHeaders })
   })
 
